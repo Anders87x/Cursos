3eb602662e26a7026d74d11ead00f4114bd79791
Anders.87 <davis_anderson_87@hotmail.com>
2020-11-10 23:25:09 -0500

    10/11/2020 Subiendo Resume por Cursos

--- view/Courses/index.php
+++ view/Courses/index.php
@@ -487,24 +487,7 @@
                     </div>
                 </div>
 
-                <div class="footer">
-                    <div class="container">
-                        <div class="uk-grid-collapse" uk-grid="">
-                            <div class="uk-width-expand@s uk-first-column">
-                                <p>© 2020 <strong>AnderCode</strong>. All Rights Reserved. </p>
-                            </div>
-                            <div class="uk-width-auto@s">
-                                <nav class="footer-nav-icon">
-                                    <ul>
-                                        <li><a href="https://www.facebook.com/AnderCode-105910337842307"><i class="icon-brand-facebook"></i></a></li>
-                                        <li><a href="https://www.youtube.com/c/AnderCode"><i class="icon-brand-youtube"></i></a></li>
-                                        <li><a href="https://github.com/Anders87x"><i class="icon-brand-github"></i></a></li>
-                                    </ul>
-                                </nav>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <?php require_once("../MainFooter/Footer.php");?>
 
             </div>
         </div>
